b02793e2ca6d1f42724a393e14715d0d6c721a9b
Laura  Rozo <admin@promoflow.net>
2025-11-10 23:49:48 -0500

    “…”

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<!-- === Favicons / Site Icon === -->
+<link rel="icon" href="view/home/img/logo_pw.png">
+
+<!-- PNGs específicos -->
+<link rel="icon" type="image/png" sizes="32x32" href="view/home/img/logo_pw.png">
+<link rel="icon" type="image/png" sizes="16x16" href="view/home/img/logo_pw.png">
+
+<!-- Apple touch icon (iOS) -->
+<link rel="apple-touch-icon" sizes="180x180" href="view/home/img/logo_pw.png">
+
+<!-- Manifest PWA (Android/Chrome) -->
+<link rel="manifest" href="/site.webmanifest">
+
+<!-- Color de UI del navegador -->
+<meta name="theme-color" content="#005548">
+
+<body>
+  <script>
+    window.location.href = 'view/home/index.php';
+  </script>
+  <noscript>
+    <a href="view/home/index.php">Ir al inicio</a>
+  </noscript>
+</body>
+</html>
