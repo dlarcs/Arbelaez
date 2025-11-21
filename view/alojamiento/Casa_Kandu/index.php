155d95a3e851a85cf5e3cbb4b6fd1a3e7eadeb65
Laura  Rozo <admin@promoflow.net>
2025-11-21 17:17:27 -0500

    “…”

--- view/alojamiento/Casa_Kandu/index.php
+++ view/alojamiento/Casa_Kandu/index.php
@@ -179,7 +179,7 @@
       "Alojamiento Casa Kandu",
       "Casa Kandu Cundinamarca",
       "Casa Kandu turismo",
-      "Casa Kandu naturaleza"
+      "Casa Kandu naturaleza",
     ]
   }
   </script>
@@ -188,11 +188,11 @@
 <body class="body_Casa_Kandu">
   <?php include "../../../view/alojamiento/Casa_Kandu/menu/menu.php" ?>
   <div class="container_Casa_Kandu">
-    <?php include "../../view/alojamiento/Casa_Kandu/title/title.php" ?>
-    <?php include "../../view/alojamiento/Casa_Kandu/slider/slider.php" ?>
-    <?php include "../../view/alojamiento/Casa_Kandu/galery/productos.php" ?>
-    <?php include "../../view/alojamiento/Casa_Kandu/bout_us/about.php" ?>
-    <?php include "../../view/alojamiento/Casa_Kandu/maps/maps.php"?>
+    <?php include "../../../view/alojamiento/Casa_Kandu/title/title.php" ?>
+    <?php include "../../../view/alojamiento/Casa_Kandu/slider/slider.php" ?>
+    <?php include "../../../view/alojamiento/Casa_Kandu/galery/productos.php" ?>
+    <?php include "../../../view/alojamiento/Casa_Kandu/bout_us/about.php" ?>
+    <?php include "../../../view/alojamiento/Casa_Kandu/maps/maps.php"?>
   </div>
   <?php include "../../../view/alojamiento/Casa_Kandu/footer/footer.php" ?>
 
