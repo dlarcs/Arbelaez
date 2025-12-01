02d97c010ac50311446b133443f2e376cfac892d
Laura  Rozo <admin@promoflow.net>
2025-11-30 23:47:41 -0500

    “…”

--- view/gastronomia/El_Huerto/title/title.php
+++ view/gastronomia/El_Huerto/title/title.php
@@ -1,14 +1,14 @@
 <?php
-$cssTime = filemtime('../../../view/gastronomia/kapaLuna360/title/title.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('../../../view/gastronomia/El_Huerto/title/title.css'); // ejemplo: '../Home/5.Video/video.css'
 $jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
 ?>
-	<link rel="stylesheet" href="../../../view/gastronomia/kapaLuna360/title/title.css?v=<?= $cssTime ?>">
+	<link rel="stylesheet" href="../../../view/gastronomia/El_Huerto/title/title.css?v=<?= $cssTime ?>">
 	<!-- title -->
 	<section class="site">
 		<div class="branding">
-			<img src="../../../view/gastronomia/kapaLuna360/title/img/logo_kapaLuna.png" alt="KapaLuna360° logo" class="logo" width="64" height="64" loading="lazy">
+			<img src="../../../view/gastronomia/El_Huerto/title/img/logo_kapaLuna.png" alt="El_Huerto° logo" class="logo" width="64" height="64" loading="lazy">
 			<div class="brand">
-				<strong class="brand-name">KapaLuna360°</strong>
+				<strong class="brand-name">Del Huerto</strong>
 				<span class="brand-tag">Gastro-Bar</span>
 			</div>
 		</div>
