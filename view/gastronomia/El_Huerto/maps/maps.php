774530def81a31ec452458bc20d8b03e6fcb38de
Laura  Rozo <admin@promoflow.net>
2025-12-01 00:12:39 -0500

    “…”

--- view/gastronomia/El_Huerto/maps/maps.php
+++ view/gastronomia/El_Huerto/maps/maps.php
@@ -8,7 +8,7 @@ $cssTime = filemtime('../../../view/gastronomia/El_Huerto/maps/maps.css'); // ej
 				<h1>¡CÓMO LLEGAR!</h1>
 
 				<div class="map_frame">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d497.3422984931704!2d-74.414152!3d4.271006!3m2!1i1024!2i768!4f13.1!4m8!3e0!4m0!4m5!1s0x8e3f1d148cf1c39f%3A0x811e2f9742baff86!2sEl_Huerto%20360%2C%20Cra.%206%20%232-11%2C%20Arbel%C3%A1ez%2C%20Cundinamarca!3m2!1d4.2709774!2d-74.41415839999999!5e0!3m2!1ses-419!2sco!4v1757083944412!5m2!1ses-419!2sco" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248.6704612676456!2d-74.4163797137858!3d4.273128030855302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f1d28c3fd6cdb%3A0x9b331c86f36712e0!2sArbel%C3%A1ez%2C%20Cundinamarca!5e0!3m2!1ses!2sco!4v1764565944400!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 				</div>
 			</div>
 		</div>
