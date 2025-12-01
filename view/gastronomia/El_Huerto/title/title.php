827e372d79784607da9264e169611ff9300c8f6f
Laura  Rozo <admin@promoflow.net>
2025-11-30 23:51:47 -0500

    “…”

--- view/gastronomia/El_Huerto/title/title.php
+++ view/gastronomia/El_Huerto/title/title.php
@@ -1,12 +1,11 @@
 <?php
 $cssTime = filemtime('../../../view/gastronomia/El_Huerto/title/title.css'); // ejemplo: '../Home/5.Video/video.css'
-$jsTime = filemtime('');   // ejemplo: '../Home/5.Video/video.js'
 ?>
 	<link rel="stylesheet" href="../../../view/gastronomia/El_Huerto/title/title.css?v=<?= $cssTime ?>">
 	<!-- title -->
 	<section class="site">
 		<div class="branding">
-			<img src="../../../view/gastronomia/El_Huerto/title/img/logo_kapaLuna.png" alt="El_Huerto° logo" class="logo" width="64" height="64" loading="lazy">
+			<img src="../../../view/gastronomia/El_Huerto/img/logo.jpg" alt="El_Huerto logo" class="logo" width="64" height="64" loading="lazy">
 			<div class="brand">
 				<strong class="brand-name">Del Huerto</strong>
 				<span class="brand-tag">Pizzeria Artesanal</span>
