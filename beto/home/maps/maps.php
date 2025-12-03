5608fa97cbb33ea6da747b7f60c527a67cff15fb
Laura  Rozo <admin@promoflow.net>
2025-12-02 19:31:36 -0500

    “…”

--- beto/home/maps/maps.php
+++ beto/home/maps/maps.php
@@ -12,12 +12,7 @@ $cssTime = filemtime('../../beto/home/maps/maps.css');
   <div class="map-card">
     <div class="map-frame" role="region" aria-label="Mapa de Google">
       <!-- Pega aquí tu iframe de Google Maps -->
-      <iframe
-        src="https://maps.app.goo.gl/rzmXcSMqgXMySY4y8"
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-        allowfullscreen>
-      </iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3976.9199550901385!2d-74.09431654232786!3d4.608350199445022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1ses!2sco!4v1764721878043!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
     <div class="map-actions">
