94523886423e508ef9ed9108c6e08149f3826c42
Laura  Rozo <admin@promoflow.net>
2025-12-14 08:18:13 -0500

    “…”

--- view/home/TourBurguer/Burguer.php
+++ view/home/TourBurguer/Burguer.php
@@ -104,12 +104,12 @@ $jsTime  = filemtime('../../view/home/TourBurguer/Burguer.js');
     <!-- CARD 2 – KapaLuna 360° -->
     <article
       class="tourburger-card"
-      data-image="../../view/home/TourBurguer/img/kapaluna0.jpeg"
+      data-image="../../view/home/TourBurguer/img/kapaluna.jpeg"
       data-image-alt="TourBurger parrillera de KapaLuna 360°"
     >
       <figure class="tourburger-card__thumb">
         <img
-          src="../../view/home/TourBurguer/img/kapaluna0.jpeg"
+          src="../../view/home/TourBurguer/img/kapaluna.jpeg"
           alt="TourBurger parrillera de KapaLuna 360°"
           loading="lazy"
           decoding="async"
