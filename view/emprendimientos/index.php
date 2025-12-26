65df4ca2609d0956444840e26c0ed1926ef982e1
Laura  Rozo <admin@promoflow.net>
2025-12-26 12:38:00 -0500

    “…”

--- view/emprendimientos/index.php
+++ view/emprendimientos/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>Emprendimientos en Arbeláez –</title>
+  <title>Emprendimientos en Arbeláez </title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- Language & robots -->
