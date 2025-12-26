9b1805214f105ddf8a017c1b9ac16aacaa08f846
Laura  Rozo <admin@promoflow.net>
2025-12-26 10:53:02 -0500

    “…”

--- view/alojamiento/Casa_Kiyari/bout_us/about.php
+++ view/alojamiento/Casa_Kiyari/bout_us/about.php
@@ -5,16 +5,6 @@ $cssTime = filemtime('../../../view/alojamiento/Casa_Kiyari/bout_us/about.css');
 	<div class="videos-grid">
   <div class="video-container">
     <iframe
-      src="../../../view/alojamiento/Casa_Kiyari/bout_us/video.mp4"
-      title="Video de ejemplo 1"
-      frameborder="0"
-      allow="autoplay; encrypted-media"
-      allowfullscreen>
-    </iframe>
-  </div>
-
-  <div class="video-container">
-    <iframe
       src="../../../view/alojamiento/Casa_Kiyari/bout_us/video1.mp4"
       title="Video de ejemplo 2"
       frameborder="0"
