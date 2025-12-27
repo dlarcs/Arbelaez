1a8ed2ddb71191efc6bca463cc13ea6759568ce8
Laura  Rozo <admin@promoflow.net>
2025-12-27 11:51:26 -0500

    “…”

--- view/artesanias/Almacen_Religioso/maps/maps.php
+++ view/artesanias/Almacen_Religioso/maps/maps.php
@@ -8,7 +8,7 @@ $cssTime = filemtime('../../../view/artesanias/Almacen_Religioso/maps/maps.css')
 				<h1>¡CÓMO LLEGAR!</h1>
 
 				<div class="map_frame">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d262.8493700853989!2d-74.41522578333561!3d4.272501220811095!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1766854064484!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3978.730395858198!2d-74.4152144!3d4.2725468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f1d5302a52be5%3A0xc24da78f696d0d5f!2sAlmacen%20Religioso%20Y%20Artesanal%20Divino%20Ni%C3%B1o%20Jes%C3%BAs!5e0!3m2!1ses!2sco!4v1766854268901!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 			</div>
 		</div>
