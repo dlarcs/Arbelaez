1ad8cd704ee311cb187335fc65eaba2af2bf8cb1
Laura  Rozo <admin@promoflow.net>
2026-01-07 10:28:29 -0500

    “…”

--- view/Proyectos/title/title.php
+++ view/Proyectos/title/title.php
@@ -5,7 +5,7 @@ $cssTime = filemtime('../../view/Proyectos/title/title.css'); // ejemplo: '../Ho
 	<!-- title -->
 	<section class="site">
 		<div class="branding">
-			<img src="../../view/global/img/logo.png" alt="Proyecto" class="logo" width="64" height="64" loading="lazy">
+			<img src="../../view/home/img/logo_pw.png" alt="Proyecto" class="logo" width="64" height="64" loading="lazy">
 			<div class="brand">
 				<strong class="brand-name">Proyectos en Arbeláez</strong>
 				<span class="brand-tag">Turismo</span>
