0f2bef5764a816f39a446996099bd4eeaf0d5850
Laura  Rozo <admin@promoflow.net>
2026-01-07 10:21:22 -0500

    “…”

--- view/Proyectos/title/title.php
+++ view/Proyectos/title/title.php
@@ -1,11 +1,11 @@
 <?php
-$cssTime = filemtime('../view/Proyectos/title/title.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('../../view/Proyectos/title/title.css'); // ejemplo: '../Home/5.Video/video.css'
 ?>
 	<link rel="stylesheet" href="../../view/Proyectos/title/title.css?v=<?= $cssTime ?>">
 	<!-- title -->
 	<section class="site">
 		<div class="branding">
-			<img src="../view/Proyectos/img/portada.png" alt="Logo Artesanías Martha E" class="logo" width="64" height="64" loading="lazy">
+			<img src="../../view/global/img/logo.png" alt="Proyecto" class="logo" width="64" height="64" loading="lazy">
 			<div class="brand">
 				<strong class="brand-name">Proyectos en Arbeláez</strong>
 				<span class="brand-tag">Turismo</span>
