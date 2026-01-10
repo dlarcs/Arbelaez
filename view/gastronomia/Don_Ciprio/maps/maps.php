6a747acabe0803eadfffd49738d3a9688ca11fa9
Laura  Rozo <admin@promoflow.net>
2026-01-09 21:40:40 -0500

    “…”

--- view/gastronomia/Don_Ciprio/maps/maps.php
+++ view/gastronomia/Don_Ciprio/maps/maps.php
@@ -8,7 +8,7 @@ $cssTime = filemtime('../../../view/gastronomia/Don_Ciprio/maps/maps.css'); // e
 				<h1>¡CÓMO LLEGAR!</h1>
 
 				<div class="map_frame">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d796.8107655833551!2d-74.4173819811929!3d4.2720714562843565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f1d28a81cb0a1%3A0x3bc28da3a9039fc8!2sAsadero%20El%20Fog%C3%B3n%20del%20Tri%C3%A1ngulo!5e0!3m2!1ses!2sco!4v1762274831552!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d994.6818377781228!2d-74.41629371048548!3d4.27313365274937!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f1d004fe62315%3A0x933e7fb625c73d81!2sDon%20Ciprio!5e0!3m2!1ses!2sco!4v1768012818150!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 			</div>
 		</div>
