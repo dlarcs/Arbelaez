9d4426019a7fdd12840d9880bf6669d8676ba36c
Laura  Rozo <admin@promoflow.net>
2026-01-12 18:29:48 -0500

    “…”

--- view/global/menu/menu.php
+++ view/global/menu/menu.php
@@ -8,7 +8,6 @@ $cssTime = filemtime('../../view/global/menu/menu.css');
 
   <!-- Marca / logo -->
   <a class="brand_menu" href="../../view/home/index.php" aria-label="Inicio">
-    <h1 class="brand-text">Logo</h1>
 
     <img src="../../view/global/img/logo.png" alt="Arbeláez" class="brand-logo">
 
