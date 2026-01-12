624c07eb7e2142ae31d6f3d50f78dcbc74d42e19
Laura  Rozo <admin@promoflow.net>
2026-01-12 18:34:07 -0500

    “…”

--- view/global/menu/menu.php
+++ view/global/menu/menu.php
@@ -6,35 +6,35 @@ $cssTime = filemtime('../../view/global/menu/menu.css');
 <header class="site-header">
   <a href="#main" class="skip-link">Saltar al contenido principal</a>
 
-  <!-- Marca / logo -->
   <a class="brand_menu" href="../../view/home/index.php" aria-label="Inicio">
-
     <img src="../../view/global/img/logo.png" alt="Arbeláez" class="brand-logo">
-
   </a>
 
+  <!-- Toggle -->
+  <input type="checkbox" id="nav-toggle" class="nav-toggle" hidden>
+
+  <!-- Botón hamburguesa -->
+  <label for="nav-toggle" class="burger" aria-label="Abrir menú" aria-controls="site-nav"></label>
+
+  <!-- Fondo oscuro para cerrar -->
+  <label for="nav-toggle" class="scrim" aria-hidden="true"></label>
+
   <!-- Navegación -->
-  <nav class="nav" aria-label="Navegación principal">
+  <nav id="site-nav" class="nav" aria-label="Navegación principal">
     <ul class="nav-list">
       <li><a href="../../view/home/index.php">Inicio</a></li>
       <li><a href="../../view/artesanias/index.php">Artesanías</a></li>
       <li><a href="../../view/alojamientos/index.php">Alojamientos</a></li>
-			<li><a href="../../view/gastronomia/index.php">Gastronomía</a></li>
-
+      <li><a href="../../view/gastronomia/index.php">Gastronomía</a></li>
 
       <li class="dropdown">
         <details>
           <summary>Más <span class="caret" aria-hidden="true">▾</span></summary>
           <ul class="dropdown-menu">
             <li><a href="../../view/history/index.php">Historia</a></li>
-            <!-- Agrega aquí lo que quieras dentro de “Más” -->
-            <!-- <li><a href="../../view/turismo/index.php">Turismo</a></li> -->
-            <!-- <li><a href="../../view/contacto/index.php">Contacto</a></li> -->
           </ul>
         </details>
       </li>
     </ul>
   </nav>
-
-
 </header>
