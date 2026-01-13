6ec1c36155b85a699adb76023e490e1b650b1295
Laura  Rozo <admin@promoflow.net>
2026-01-12 22:10:06 -0500

    “…”

--- view/home/intro/intro.php
+++ view/home/intro/intro.php
@@ -37,18 +37,18 @@ $cssTime = @filemtime($cssPath) ?: time(); // fallback si falta el archivo
     </a>
 
     <!-- Instagram -->
-    <a class="post-share__btn" href="#" aria-label="Instagram" title="Instagram">
+    <a class="post-share__btn" href="https://www.instagram.com/arbelaez_turismo/" aria-label="Instagram" title="Instagram">
       <svg viewBox="0 0 24 24" aria-hidden="true">
         <path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9A5.5 5.5 0 0 1 16.5 22h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2Zm0 2A3.5 3.5 0 0 0 4 7.5v9A3.5 3.5 0 0 0 7.5 20h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 16.5 4h-9Zm10.25 1.75a1 1 0 1 1 0 2 1 1 0 0 1 0-2ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z"/>
       </svg>
     </a>
 
     <!-- TikTok -->
-    <a class="post-share__btn" href="#" aria-label="TikTok" title="TikTok">
+    <!-- <a class="post-share__btn" href="#" aria-label="TikTok" title="TikTok">
       <svg viewBox="0 0 24 24" aria-hidden="true">
         <path d="M16.5 2c.4 3 2.4 5 5.5 5v3c-1.9 0-3.6-.6-5-1.7V16c0 3.3-2.7 6-6 6s-6-2.7-6-6 2.7-6 6-6c.4 0 .9.05 1.3.15V13.3c-.4-.2-.8-.3-1.3-.3-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3V2h2.5Z"/>
       </svg>
-    </a>
+    </a> -->
   </nav>
 
     </div>
