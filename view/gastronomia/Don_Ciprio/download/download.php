41aa9444aa84f24f78438954d02b391948bc08f6
Laura  Rozo <admin@promoflow.net>
2026-01-12 22:29:43 -0500

    “…”

--- view/gastronomia/Don_Ciprio/download/download.php
+++ view/gastronomia/Don_Ciprio/download/download.php
@@ -5,7 +5,7 @@ $cssTime = filemtime('../../../view/gastronomia/Don_Ciprio/download/download.css
 	<link rel="stylesheet" href="../../../view/gastronomia/Don_Ciprio/download/download.css?v=<?= $cssTime ?>">
 	<section id="menu_Don_Ciprio" class="card_download">
 		<div class="download">
-			<a href="../../../pdf" download="Menu-Restaurante.pdf">
+			<a href="../../../view/gastronomia/Don_Ciprio/download/menu.pdf" download="Menu-Restaurante.pdf">
 				<h2>Haz click aqui y <br>Descarga el Menú</h2>
 			</a>
 		</div>
