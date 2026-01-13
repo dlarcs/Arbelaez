65a30b6a112f927b53689fc802f39b4d85cdc314
Laura  Rozo <admin@promoflow.net>
2026-01-12 19:46:13 -0500

    “…”

--- view/home/intro/intro.php
+++ view/home/intro/intro.php
@@ -18,9 +18,9 @@ $cssTime = @filemtime($cssPath) ?: time(); // fallback si falta el archivo
           alt="Avatar del autor"
         />
         <div class="post-author__text">
-          <div class="post-author__name">Equipo Pueblo</div>
+          <div class="post-author__name">Arbeláez Digilalizada</div>
           <div class="post-author__sub">
-            <time datetime="2025-01-20">20 de enero 2025</time>
+            <time datetime="2025-01-20">25 de octubre 2025</time>
             <span class="dot">•</span>
             <span>7 minutos lectura</span>
           </div>
@@ -28,35 +28,36 @@ $cssTime = @filemtime($cssPath) ?: time(); // fallback si falta el archivo
       </div>
 
       <nav class="post-share" aria-label="Compartir">
-        <a class="post-share__btn" href="#" aria-label="Copiar enlace" title="Copiar enlace">
-          <!-- link -->
-          <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M10.59 13.41a1 1 0 0 1 0-1.41l3.17-3.17a3 3 0 1 1 4.24 4.24l-1.59 1.59a3 3 0 0 1-4.24 0 .999.999 0 1 1 1.41-1.41 1 1 0 0 0 1.41 0l1.59-1.59a1 1 0 1 0-1.41-1.41l-3.17 3.17a1 1 0 0 1-1.41 0Z"/>
-            <path d="M13.41 10.59a1 1 0 0 1 0 1.41l-3.17 3.17a3 3 0 1 1-4.24-4.24l1.59-1.59a3 3 0 0 1 4.24 0 .999.999 0 1 1-1.41 1.41 1 1 0 0 0-1.41 0L7.42 12.34a1 1 0 1 0 1.41 1.41l3.17-3.17a1 1 0 0 1 1.41 0Z"/>
-          </svg>
-        </a>
+    <!-- Copiar enlace (opcional) -->
+    <a class="post-share__btn" href="#" aria-label="Copiar enlace" title="Copiar enlace">
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M10.59 13.41a1 1 0 0 1 0-1.41l3.17-3.17a3 3 0 1 1 4.24 4.24l-1.59 1.59a3 3 0 0 1-4.24 0 .999.999 0 1 1 1.41-1.41 1 1 0 0 0 1.41 0l1.59-1.59a1 1 0 1 0-1.41-1.41l-3.17 3.17a1 1 0 0 1-1.41 0Z"/>
+        <path d="M13.41 10.59a1 1 0 0 1 0 1.41l-3.17 3.17a3 3 0 1 1-4.24-4.24l1.59-1.59a3 3 0 0 1 4.24 0 .999.999 0 1 1-1.41 1.41 1 1 0 0 0-1.41 0L7.42 12.34a1 1 0 1 0 1.41 1.41l3.17-3.17a1 1 0 0 1 1.41 0Z"/>
+      </svg>
+    </a>
 
-        <a class="post-share__btn" href="#" aria-label="Compartir en LinkedIn" title="LinkedIn">
-          <!-- linkedin -->
-          <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M6.94 6.5A2.19 2.19 0 1 1 7 2.12a2.19 2.19 0 0 1-.06 4.38ZM5.5 21h2.9V8.5H5.5V21ZM10.75 8.5H13.5v1.7h.04c.38-.72 1.3-1.48 2.68-1.48 2.86 0 3.39 1.88 3.39 4.33V21h-2.9v-7.1c0-1.69-.03-3.86-2.35-3.86-2.35 0-2.71 1.84-2.71 3.74V21h-2.9V8.5Z"/>
-          </svg>
-        </a>
+    <!-- Facebook -->
+    <a class="post-share__btn" href="#" aria-label="Facebook" title="Facebook">
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M13.5 22v-8h2.7l.4-3H13.5V9.1c0-.87.24-1.47 1.5-1.47h1.6V4.95c-.28-.04-1.26-.12-2.4-.12-2.37 0-4 1.45-4 4.12V11H7.6v3h2.6v8h3.3Z"/>
+      </svg>
+    </a>
 
-        <a class="post-share__btn" href="#" aria-label="Compartir en X" title="X">
-          <!-- X -->
-          <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M18.9 2H22l-6.77 7.73L23 22h-6.5l-5.1-7.9L4.6 22H1.5l7.25-8.28L1 2h6.65l4.62 7.05L18.9 2Zm-1.14 18h1.72L6.7 3.9H4.86L17.76 20Z"/>
-          </svg>
-        </a>
+    <!-- Instagram -->
+    <a class="post-share__btn" href="#" aria-label="Instagram" title="Instagram">
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9A5.5 5.5 0 0 1 16.5 22h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2Zm0 2A3.5 3.5 0 0 0 4 7.5v9A3.5 3.5 0 0 0 7.5 20h9a3.5 3.5 0 0 0 3.5-3.5v-9A3.5 3.5 0 0 0 16.5 4h-9Zm10.25 1.75a1 1 0 1 1 0 2 1 1 0 0 1 0-2ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 2a3 3 0 1 0 0 6 3 3 0 0 0 0-6Z"/>
+      </svg>
+    </a>
 
-        <a class="post-share__btn" href="#" aria-label="Compartir en Facebook" title="Facebook">
-          <!-- facebook -->
-          <svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M13.5 22v-8h2.7l.4-3H13.5V9.1c0-.87.24-1.47 1.5-1.47h1.6V4.95c-.28-.04-1.26-.12-2.4-.12-2.37 0-4 1.45-4 4.12V11H7.6v3h2.6v8h3.3Z"/>
-          </svg>
-        </a>
-      </nav>
+    <!-- TikTok -->
+    <a class="post-share__btn" href="#" aria-label="TikTok" title="TikTok">
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M16.5 2c.4 3 2.4 5 5.5 5v3c-1.9 0-3.6-.6-5-1.7V16c0 3.3-2.7 6-6 6s-6-2.7-6-6 2.7-6 6-6c.4 0 .9.05 1.3.15V13.3c-.4-.2-.8-.3-1.3-.3-1.7 0-3 1.3-3 3s1.3 3 3 3 3-1.3 3-3V2h2.5Z"/>
+      </svg>
+    </a>
+  </nav>
+
     </div>
 
   </div>
