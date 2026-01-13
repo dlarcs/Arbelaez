24712cd11c68710cc5135cd8a313994670bc109e
Laura  Rozo <admin@promoflow.net>
2026-01-13 18:04:20 -0500

    “…”

--- view/home/slider/slider.php
+++ view/home/slider/slider.php
@@ -12,7 +12,7 @@ $jsTime  = filemtime('../../view/home/slider/slider.js');
       <article id="slide-1" class="slide is-current" aria-roledescription="slide" aria-label="1 de 4">
         <img src="../../view/home/TourBurguer/img/general2.png" alt="Montañas al amanecer">
         <div class="slide__caption">
-          <h2>Del 14-28 Diciembre 2025</h2>
+          <h2>Hasta el 25 Enero 2026</h2>
           <a href="#tourburger"><p>Para mas información da click aqui</p></a>
         </div>
       </article>
