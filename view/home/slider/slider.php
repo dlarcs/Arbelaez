4aba85a053c8419ff88a4c602722fbb63137c311
Laura  Rozo <admin@promoflow.net>
2026-01-28 01:52:19 -0500

    “…”

--- view/home/slider/slider.php
+++ view/home/slider/slider.php
@@ -12,8 +12,8 @@ $jsTime  = filemtime('../../view/home/slider/slider.js');
       <article id="slide-1" class="slide is-current" aria-roledescription="slide" aria-label="1 de 4">
         <img src="../../view/home/img/tourganador.png" alt="Montañas al amanecer">
         <div class="slide__caption">
-          <h2>Ganador Tourburger Arbeláez 2025</h2>
-          <a href="#tourburger"><p>Don Ciprio, en la casa de los pajaros</p></a>
+          <h2>Don Ciprio</h2>
+          <a href="#tourburger"><p>, Ubicación Casa de los Pajaros</p></a>
         </div>
       </article>
 
