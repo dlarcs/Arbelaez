8f61bed02c9a239975d077438bb676c320366dfa
Laura  Rozo <admin@promoflow.net>
2026-01-27 23:42:16 -0500

    “…”

--- view/home/places/places.php
+++ view/home/places/places.php
@@ -463,7 +463,7 @@ $cssTime = @filemtime($cssPath) ?: time(); // fallback si falta el archivo
               <span class="leaf"></span><span class="leaf r"></span>
             </div>
             <hr>
-            <p class="winner-name"><em>Don Ciprio</em></p>
+            <p class="winner-name">Don Ciprio </p>
             <div class="pedestal" aria-hidden="true"></div>
           </div>
         </div>
