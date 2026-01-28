f88c53cb018945301d9b262a96dfc175311f431b
Laura  Rozo <admin@promoflow.net>
2026-01-28 01:16:03 -0500

    “…”

--- view/home/video/video.php
+++ view/home/video/video.php
@@ -5,7 +5,7 @@ $cssTime = @filemtime($cssPath) ?: time(); // fallback si falta el archivo
 ?>
 <link rel="stylesheet" href="<?= $cssPath ?>?v=<?= $cssTime ?>">
 
-<video class="video_box"controls playsinline muted preload="metadata" poster="poster.jpg">
+<video controls playsinline muted preload="metadata" poster="poster.jpg">
   <source src="../../view/home/img/VIDEO.MP4" type="video/mp4">
   <source src="../../view/home/img/VIDEO.MP4" type="video/webm">
   Tu navegador no soporta video HTML5.
