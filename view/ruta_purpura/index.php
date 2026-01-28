bc7c794a15dd5532af4966c9e84e20dcd2e15551
Laura  Rozo <admin@promoflow.net>
2026-01-28 16:21:41 -0500

    “…”

---
+++ view/ruta_purpura/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title>Radio de Arbelaez</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://unpkg.com/ionicons@latest/dist/ionicons.js"></script>
+    <link rel="stylesheet" href="../../view/ruta_purpura/style.css">
+  </head>
+
+  <body class="body_Radio">
+    <?php include "../../view/global/menu/menu.php" ?>
+
+    <div class="container_Radio">
+      <?php include "../../view/Radio/slider/slider.php" ?>
+      <?php include "../../view/ruta_purpura/index.php" ?>
+      <?php include "../../view/Radio/footer/footer.php" ?>
+    </div>
+</body>
+</html>
