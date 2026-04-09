175b1b7bf2c727f3c453324e8a64fb21b0212b82
Laura  Rozo <admin@promoflow.net>
2026-04-08 21:32:52 -0500

    “…”

--- alojamiento/Cachorros_LyC/menu/menu.php
+++ alojamiento/Cachorros_LyC/menu/menu.php
@@ -1,10 +1,10 @@
 <?php
-$cssTime = filemtime('../alojamiento/Cachorros_LyC/menu/menu.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('../../alojamiento/Cachorros_LyC/menu/menu.css'); // ejemplo: '../../Home/5.Video/video.css'
 ?>
-	<link rel="stylesheet" href="../alojamiento/Cachorros_LyC/menu/menu.css?v=<?= $cssTime ?>">
+	<link rel="stylesheet" href="../../alojamiento/Cachorros_LyC/menu/menu.css?v=<?= $cssTime ?>">
 	<!-- ===== HEADER PRINCIPAL DE LA PÁGINA ===== -->
 	<header class="site-header">
-  <a class="brand_menu" href="../home/index.php" aria-label="Inicio">
+  <a class="brand_menu" href="../../home/index.php" aria-label="Inicio">
     <h1 class="brand-text">Cachorros L & C</h1>
   </a>
 
@@ -16,11 +16,11 @@ $cssTime = filemtime('../alojamiento/Cachorros_LyC/menu/menu.css'); // ejemplo: 
   <!-- Navegación -->
   <nav id="site-nav" class="nav">
     <ul class="nav-list">
-      <li><a href="../home/index.php">Inicio</a></li>
+      <li><a href="../../home/index.php">Inicio</a></li>
       <li><a href="#producto_Cachorros_LyC">Galería</a></li>
       <li><a href="#about_Cachorros_LyC">Nosotros</a></li>
       <li><a href="#ubicacion_Cachorros_LyC">Ubicación</a></li>
-      <li><a class="cta" href="../alojamiento/index.php">Volver</a></li>
+      <li><a class="cta" href="../../alojamiento/index.php">Volver</a></li>
     </ul>
   </nav>
 </header>
--- alojamiento/Cachorros_LyC/title/title.php
+++ alojamiento/Cachorros_LyC/title/title.php
@@ -1,11 +1,11 @@
 <?php
-$cssTime = filemtime('../alojamiento/Cachorros_LyC/title/title.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('../../alojamiento/Cachorros_LyC/title/title.css'); // ejemplo: '../../Home/5.Video/video.css'
 ?>
-	<link rel="stylesheet" href="../alojamiento/Cachorros_LyC/title/title.css?v=<?= $cssTime ?>">
+	<link rel="stylesheet" href="../../alojamiento/Cachorros_LyC/title/title.css?v=<?= $cssTime ?>">
 	<!-- title -->
 	<section class="site">
 		<div class="branding">
-			<img src="../alojamiento/Cachorros_LyC/img/logo.jpg" alt="kalu logo" class="logo" width="64" height="64" loading="lazy">
+			<img src="../../alojamiento/Cachorros_LyC/img/logo.jpg" alt="kalu logo" class="logo" width="64" height="64" loading="lazy">
 			<div class="brand">
 				<strong class="brand-name">Cachorros L & C</strong>
 				<span class="brand-tag">Camping, vista y comida</span>
