c55b9fcbcdd3e24aa8a3ee3314f7cfdddf2d68b3
Laura  Rozo <admin@promoflow.net>
2026-04-08 21:32:07 -0500

    “…”

--- alojamiento/Cachorros_LyC/maps/maps.php
+++ alojamiento/Cachorros_LyC/maps/maps.php
@@ -1,7 +1,7 @@
 <?php
-$cssTime = filemtime('../alojamiento/Cachorros_LyC/maps/maps.css'); // ejemplo: '../Home/5.Video/video.css'
+$cssTime = filemtime('../../alojamiento/Cachorros_LyC/maps/maps.css'); // ejemplo: '../../Home/5.Video/video.css'
 ?>
-	<link rel="stylesheet" href="../alojamiento/Cachorros_LyC/maps/maps.css?v=<?= $cssTime ?>">
+	<link rel="stylesheet" href="../../alojamiento/Cachorros_LyC/maps/maps.css?v=<?= $cssTime ?>">
 	<section id="ubicacion_Cachorros_LyC" class="seccion_maps">
 		<div class="maps">
 			<div class="text_maps">
