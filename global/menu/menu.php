0de2926315beb6d4c5677e24520f0c979dcf9946
Laura  Rozo <admin@promoflow.net>
2026-04-23 16:41:42 -0500

    “…”

--- global/menu/menu.php
+++ global/menu/menu.php
@@ -1,7 +1,7 @@
 <?php
-$cssTime = filemtime('../global/menu/menu.css'); // ejemplo: '../5.Video/video.css'
+$cssTime = filemtime('../../global/menu/menu.css'); // ejemplo: '../5.Video/video.css'
 ?>
-	<link rel="stylesheet" href="../global/menu/menu.css?v=<?= $cssTime ?>">
+	<link rel="stylesheet" href="../../global/menu/menu.css?v=<?= $cssTime ?>">
 	<!-- ===== HEADER PRINCIPAL DE LA PÁGINA ===== -->
 	<!-- HEADER -->
 	<header class="site-header">
@@ -22,12 +22,12 @@ $cssTime = filemtime('../global/menu/menu.css'); // ejemplo: '../5.Video/video.c
 		<!-- Navegación -->
 		<nav id="site-nav" class="nav">
 			<ul class="nav-list">
-				<li><a href="../index.php">Inicio</a></li>
-				<li><a href="../artesanias/Almacen_Religioso/index.php">Artesanías</a></li>
-				<li><a href="../alojamiento/index.php">Alojamiento</a></li>
-				<li><a href="../gastronomia/index.php">Gastronomía</a></li>
-				<li><a href="../rio/index.php">Zonas de agua</a></li>
-				<li><a href="../festividades/index.php">Festividades</a></li>
+				<li><a href="../../index.php">Inicio</a></li>
+				<li><a href="../../artesanias/Almacen_Religioso/index.php">Artesanías</a></li>
+				<li><a href="../../alojamiento/index.php">Alojamiento</a></li>
+				<li><a href="../../gastronomia/index.php">Gastronomía</a></li>
+				<li><a href="../../rio/index.php">Zonas de agua</a></li>
+				<li><a href="../../festividades/index.php">Festividades</a></li>
 			</ul>
 		</nav>
 	</header>
