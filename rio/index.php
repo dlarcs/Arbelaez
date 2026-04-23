b37b570a9522829c24a117bf0aa34698bb1fc495
Laura  Rozo <admin@promoflow.net>
2026-04-23 17:31:13 -0500

    “…”

--- rio/index.php
+++ rio/index.php
@@ -85,10 +85,11 @@
     <?php include "../rio/footer/footer.php" ?>
 
   </div>
-  <section class="scroll-indicator" aria-label="More content below">
-    <span class="scroll-indicator__circle">
-      <span class="scroll-indicator__arrow"></span>
-    </span>
-  </section>
+
 </body>
+<section class="scroll-indicator" aria-label="More content below">
+  <span class="scroll-indicator__circle">
+    <span class="scroll-indicator__arrow"></span>
+  </span>
+</section>
 </html>
