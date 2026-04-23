1afb87a975c8b35cfa5bd6cc74a2ffa132ec10ba
Laura  Rozo <admin@promoflow.net>
2026-04-23 15:15:05 -0500

    “…”

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $jsTime  = filemtime($base . '/home/app.js');
   <?php include $base . "/home/intro/intro.php"; ?>
   <?php include $base . "/home/places/places.php"; ?>
   <?php include $base . "/home/imagen/imagen.php"; ?>
-  <?php include $base . "/home/text/text.php"; ?>
+  <?php include $base . "/home/Rifa/rifa.php" ?>
   <?php include $base . "/home/maps/maps.php"; ?>
 </div>
   <?php include $base . "/global/footer/footer.php"; ?>
