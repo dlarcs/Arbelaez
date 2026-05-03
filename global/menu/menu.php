7fd60d987ca06befa320f0046ff2043e6b4a3fa1
Laura  Rozo <admin@promoflow.net>
2026-05-03 14:54:07 -0500

    “…”

--- global/menu/menu.php
+++ global/menu/menu.php
@@ -1,28 +1,36 @@
 <?php
-$cssFile = '../global/menu/menu.css';
-$cssTime = is_file($cssFile) ? filemtime($cssFile) : '';
+$cssTime = filemtime('../global/menu/menu.css');
+// $jsTime = filemtime('../../');
 ?>
-<link rel="stylesheet" href="<?= $cssFile ?><?= $cssTime ? '?v=' . $cssTime : '' ?>">
-
+<link rel="stylesheet" href="../global/menu/menu.css?v=<?= $cssTime ?>">
 <header class="site-header">
   <a class="brand_menu" href="../index.php" aria-label="Inicio">
-    Arbeláez Cundinamarca
+    <span class="brand-text">Arbeláez Cundinamarca</span>
   </a>
 
   <input type="checkbox" id="nav-toggle" class="nav-toggle" hidden>
 
-  <label for="nav-toggle" class="burger" aria-label="Abrir menú">
-    <span></span><span></span><span></span>
+  <label
+    for="nav-toggle"
+    class="burger"
+    aria-label="Abrir menú"
+    aria-controls="site-nav"
+  >
+    <span></span>
+    <span></span>
+    <span></span>
   </label>
 
   <label for="nav-toggle" class="scrim" aria-hidden="true"></label>
 
-  <nav class="nav" aria-label="Navegación principal">
-    <a href="../index.php">Inicio</a>
-    <a href="../artesanias/index.php">Artesanías</a>
-    <a href="../alojamiento/index.php">Alojamiento</a>
-    <a href="../gastronomia/index.php">Gastronomía</a>
-    <a href="../rio/index.php">Zonas de agua</a>
-    <a href="../festividades/index.php">Festividades</a>
+  <nav id="site-nav" class="nav" aria-label="Navegación principal">
+    <ul class="nav-list">
+      <li><a href="../index.php">Inicio</a></li>
+      <li><a href="../artesanias/index.php">Artesanías</a></li>
+      <li><a href="../alojamiento/index.php">Alojamiento</a></li>
+      <li><a href="../gastronomia/index.php">Gastronomía</a></li>
+      <li><a href="../rio/index.php">Zonas de agua</a></li>
+      <li><a href="../festividades/index.php">Festividades</a></li>
+    </ul>
   </nav>
 </header>
