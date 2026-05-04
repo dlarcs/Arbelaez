05315968f9df622f21b0fe19ae2e845f32eb1bfa
Laura  Rozo <admin@promoflow.net>
2026-05-04 08:44:58 -0500

    “…”

--- home/intro/intro.php
+++ home/intro/intro.php
@@ -19,7 +19,7 @@ $cssVer = is_file($cssFile) ? filemtime($cssFile) : '';
           <div class="post-author__sub">
             <time datetime="2025-01-20">25 de octubre 2025</time>
             <span class="dot">•</span>
-            <span>
+            <span class="description_intro">
   Descubre Arbeláez, Cundinamarca, un destino turístico cerca de Bogotá ideal para disfrutar de la naturaleza, los ríos, la gastronomía local y el descanso.
   En Arbelaez.com.co encontrarás alojamiento, artesanías, restaurantes y los mejores lugares para visitar en Arbeláez, desde zonas de agua hasta experiencias culturales y actividades al aire libre.
   Explora todo lo que este hermoso municipio tiene para ofrecer y planifica tu próxima visita.
